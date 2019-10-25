60188e2170bb550f440150fe2f674be349aa055f
Robson Rodrigues <huazin@hotmail.com>
2019-10-25 19:35:59 -0300

    Atividades Concluidas

---
+++ MyBlog/Models/PushMessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Lib.Net.Http.WebPush;
+
+namespace MyBlog.Controllers
+{
+    public class PushMessageViewModel
+    {
+        public string Topic { get; set; }
+
+        public string Notification { get; set; }
+
+        public PushMessageUrgency Urgency { get; set; } = PushMessageUrgency.Normal;
+    }
+}
---
+++ MyBlog/Services/IBlogService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MyBlog.Controllers
+{
+    public interface IBlogService
+    {
+        List<BlogPost> GetLatestPosts();
+        string GetPostText(string link);
+
+        List<BlogPost> GetOlderPosts(int oldestPostId);
+    }
+}
